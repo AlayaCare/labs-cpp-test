095729d004e7e314d1ab64dbc2de8143b9f05d6a
Francois Lacoursiere <francois.lacoursiere@alayacare.com>
2021-11-08 09:20:41 -0500

    Update FrequencyCounter.h

--- source/FrequencyCounter.h
+++ source/FrequencyCounter.h
@@ -6,12 +6,19 @@
 
 class FrequencyCounter {
 public:
+  /***
+  FrequencyCounter count the number tokens in a list of tokens
+  
+  For example for the input [ 'a', 'b', 'c', 'a', 'c', 'e']
+  The output of getFrequency would be
+  { {a,2}, {b, 1}, {c, 2}, {e, 1}}
+  */
   FrequencyCounter(std::vector<std::string> tokens);
 
   /***
-   * Returns a map for term frequency
+   * Returns a map for token frequency
    * key: token
-   * value: count of term
+   * value: count
    */
   std::map<std::string, int> getFrequency();
 };
